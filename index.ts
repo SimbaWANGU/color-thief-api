c7e69f287312173e5ae5b136010f90993c10a137
SimbaWANGU <simbawangulu@gmail.com>
2024-02-05 12:52:41 +0300

    complete initial setup

---
+++ index.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import type { Request, Response } from 'express'
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+const port = 3000
+
+// *Middleware
+app.use(cors())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+// *Routes
+app.use('/', (req: Request, res: Response) => {
+  res.json({
+    hello: 'Welcome to the app'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Express server listening at http://localhost:${port}`)
+})
